6d9808e1c35beb4c2c1bc8c195fed8453a593219
BlueSapphy <bluesapphyoowoo@gmail.com>
2023-11-09 14:41:19 -0500

    revise itemgroup

--- src/main/java/net/bluesapphy/stardewdelight/item/SDItemGroup.java
+++ src/main/java/net/bluesapphy/stardewdelight/item/SDItemGroup.java
@@ -11,21 +11,22 @@ import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 
 public class SDItemGroup {
+    public static ItemGroup STARDEW_DELIGHT;
 
-    public static final ItemGroup STARDEW_DELIGHT = Registry.register(Registries.ITEM_GROUP,
-            new Identifier(StardewDelight.MOD_ID,"hazelnut"),
-            FabricItemGroup.builder().displayName(Text.translatable("itemgroup.hazelnut"))
-                    .icon(()-> new ItemStack(SDItemRegistry.HAZELNUT)).entries((displayContext, entries) -> {
-                     entries.add(SDItemRegistry.HAZELNUT);
-                     entries.add(SDItemRegistry.PARSNIP);
-                     entries.add(SDItemRegistry.PARSNIP_SEEDS);
+        public static void registerItemGroups() {
+            StardewDelight.LOGGER.info("Registering Item Groups for " + StardewDelight.MOD_ID);
+            STARDEW_DELIGHT = Registry.register(Registries.ITEM_GROUP,
+                    new Identifier(StardewDelight.MOD_ID, "hazelnut"),
+                    FabricItemGroup.builder().displayName(Text.translatable("itemgroup.hazelnut"))
+                            .icon(() -> new ItemStack(SDItemRegistry.HAZELNUT)).entries((displayContext, entries) -> {
+                                entries.add(SDItemRegistry.HAZELNUT);
+                                entries.add(SDItemRegistry.PARSNIP);
+                                entries.add(SDItemRegistry.PARSNIP_SEEDS);
 
 
-                     entries.add(SDBlockRegistry.HAZELNUT_BAG);
-                     entries.add(SDBlockRegistry.WILD_PARSNIP);
+                                entries.add(SDBlockRegistry.HAZELNUT_BAG);
+                                entries.add(SDBlockRegistry.WILD_PARSNIP);
 
-                    }).build());
-    public static void registerItemGroups(){
-        StardewDelight.LOGGER.info("Registering Item Groups for "+ StardewDelight.MOD_ID);
+                            }).build());
+        }
     }
-}
